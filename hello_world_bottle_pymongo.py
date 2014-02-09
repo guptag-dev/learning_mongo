693348480b24960bd6bbfd20ee885915f1b4066a
gg <1989.gaurav@gmail.com>
2014-02-08 17:54:43 -0800

    adding week 1 files

---
+++ hello_world_bottle_pymongo.py
@@ -0,0 +1,13 @@
+
+import bottle
+import pymongo
+
+@bottle.route('/')
+def index():
+	connection = pymongo.MongoClient('localhost', 27017)
+	db = connection.test
+	names = db.names
+	item = names.find_one()
+	return '<b> Hello %s! </b>' %item['nick']
+
+bottle.run(host='localhost', port=8182)
